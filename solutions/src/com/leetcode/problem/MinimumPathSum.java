0712941407b379682fbd04943c882132d3c974d6
Halil AYYILDIZ <halil.ayyildiz@siemens.com>
2017-08-11 13:41:43 +0300

    update
    Signed-off-by: Halil AYYILDIZ <halil.ayyildiz@siemens.com>

---
+++ solutions/src/com/leetcode/problem/MinimumPathSum.java
@@ -0,0 +1,22 @@
+package com.leetcode.problem;
+
+public class MinimumPathSum
+{
+	public int minPathSum(int[][] grid)
+	{
+
+		
+		
+		
+		
+		return -1;
+	}
+
+	public static void main(String[] args)
+	{
+		int[][] grid = { { 1, 2, 3, 4, 5 }, { 30, 4, 5, 2, 1 }, { 10, 23, 1, 1, 1 }, { 0, 0, 3, 4, 5 }, { 4, 12, 50, 20000, 1 } };
+		new MinimumPathSum().minPathSum(grid);
+
+	}
+
+}
