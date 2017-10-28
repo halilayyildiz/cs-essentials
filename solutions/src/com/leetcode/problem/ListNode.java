c3b3668f6c46ae9cf0a1e29ff160deeabcf3aff6
Halil AYYILDIZ <halil.ayyildiz@gmail.com>
2017-10-28 11:54:02 +0300

    update

--- solutions/src/com/leetcode/problem/ListNode.java
+++ solutions/src/com/leetcode/problem/ListNode.java
@@ -2,11 +2,11 @@ package com.leetcode.problem;
 
 public class ListNode
 {
-	int			val;
-	ListNode	next;
+    int val;
+    ListNode next;
 
-	ListNode(int x)
-	{
-		val = x;
-	}
+    ListNode(int x)
+    {
+        val = x;
+    }
 }
