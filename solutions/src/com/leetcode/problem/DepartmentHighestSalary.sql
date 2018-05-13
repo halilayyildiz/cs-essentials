caa765cf72d11718016c3a756793f2d8f9f0b123
Halil AYYILDIZ <halil.ayyildiz@gmail.com>
2018-05-13 23:11:24 +0300

    update

--- solutions/src/com/leetcode/problem/DepartmentHighestSalary.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  d.Name AS Department, e.Name AS EMPLOYEE, e.Salary AS SALARY 
-FROM 
-  Employee e, 
-  Department d,
-  (SELECT e.DepartmentId AS DID, MAX(Salary) as MSALARY FROM Employee e GROUP BY DepartmentId) m
-WHERE 
-  e.Salary = m.MSALARY AND e.DepartmentId = m.DID AND d.Id= m.DID;
--- solutions/src/com/leetcode/problem/ExchangeSeats.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-  ids.new_id AS id, (SELECT student FROM seat WHERE id = ids.old_id) AS student
-FROM 
-  (SELECT id as old_id, IF(id % 2 <> 0, IF(id = (SELECT MAX(id) FROM seat), id, id+1), id-1) AS new_id FROM seat) ids
-ORDER BY
-  ids.new_id
