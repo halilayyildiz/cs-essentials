8fcde44a115e780fe479af957bb1bf7a8d7e10e1
Halil AYYILDIZ <halil.ayyildiz@gmail.com>
2018-08-15 23:02:10 +0300

    update

--- src/main/java/com/halilayyildiz/Question1.java
+++ src/main/java/com/halilayyildiz/Question1.java
@@ -1,27 +1,63 @@
 package com.halilayyildiz;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Question1
 {
-    public String solve()
+    public int solution(int[] T)
     {
-        Map<String, Integer> map = new HashMap<>();
-
-        if (map.putIfAbsent("testKey", 0) != null)
+        if (T.length < 3)
         {
-            map.put("testKey", map.get("testKey") + 1);
+            return 1;
         }
 
-        return "";
+        int[] rightMin = new int[T.length];
+
+        for (int i = T.length - 1, min = T[T.length - 1]; i >= 0; i--)
+        {
+            if (T[i] < min)
+            {
+                min = T[i];
+            }
+            rightMin[i] = min;
+        }
+
+        for (int i = 0, max = T[0]; i < T.length; i++)
+        {
+            if (rightMin[i] > max)
+            {
+                return i;
+            }
+            else
+            {
+                max = Math.max(max, T[i]);
+            }
+        }
+
+        return 0;
     }
 
     public static void main(String[] args)
     {
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 3, 3, 6, 0, 9 }));
 
-        String result = new Question1().solve();
-        System.out.println("Result: " + result);
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 3, 3, 6 }));
+
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 1, 3, 6 }));
+
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 1, 2 }));
+
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 1, -2, 6 }));
+
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { 5, -2, 3, 8, 6 }));
+
+        System.out.println("Result: " + new Question1().solution(new int[]
+        { -5, -5, -5, -42, 6, 12 }));
+
     }
 
 }
